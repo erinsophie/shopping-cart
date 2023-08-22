8e054231cb562347266d7220e08907bb161da033
Sophie Little <119007056+erinsophie@users.noreply.github.com>
2023-08-22 21:08:14 +0100

    Remove basket upon checkout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import { useBasket } from './components/BasketContext';
 import Header from '../src/components/Header';
 import Footer from '../src/components/Footer';
 import Basket from './components/Basket';
 
 function App() {
-  const { showBasket } = useBasket();
+  const { showBasket, setShowBasket } = useBasket();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === '/checkout') {
+      setShowBasket(false);
+    }
+  }, [location.pathname, setShowBasket]);
 
   return (
     <div>
